55740c6b776270300b403800d33aee27053d63a3
Anushka Verma <anushkaverma779@gmail.com>
2023-02-16 16:53:34 +0530

    done

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ button.addEventListener("click", function() {
     let div = document.getElementById("app");
     if(div.className=="day") {
         div.className = "night";
+        button.className = "button_night";
     } else {
         div.className = "day";
+        button.className = "button_day";
     }
 })
 
